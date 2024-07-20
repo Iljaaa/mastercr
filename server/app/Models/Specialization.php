9a2e0c34bd29c0c139063b9d1d6de1b18397c6d1
ilia <the.ilja@gmail.com>
2024-07-20 10:24:16 +0100

    additional modals

--- server/app/Models/Specialization.php
+++ server/app/Models/Specialization.php
@@ -5,6 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property int $id
+ * @property string $specialization
+ */
 class Specialization extends Model
 {
     use HasFactory;
